2abd12525010f0c46b45da95787cce623884c432
Atikah <atikahnazir6@gmail.com>
2024-07-27 13:12:54 +0800

    route

---
+++ app/collection/page.js
@@ -0,0 +1,16 @@
+//import data from public/data
+// use data into component ProductList
+
+import ProductList from "../../components/product/ProductList";
+import products from "../../public/data/products.json";
+
+export default function Collection() {
+  return (
+    <>
+      <div className="px-5">
+        <h1 className="py-3">Collection</h1>
+        <ProductList products={products} />
+      </div>
+    </>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,17 +1,56 @@
 import Image from "next/image";
 import heroImage from "../public/images/hero-image.jpg";
+import pic1 from "../public/images/pexels-cottonbro-4064835.jpg";
+import pic2 from "../public/images/pexels-pnw-prod-8490187.jpg";
+import ProductList from "@/components/product/ProductList";
+import ProductCard from "@/components/product/ProductCard";
+import products from "../public/data/products.json";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className=" bg-lime-300 border border-red-700  ">
+    <main>
       <div>
         {/* <Image src="/images/hero-image.jpg" alt="hero" width={500} height={500} /> */}
         <Image src={heroImage} alt="hero" />
       </div>
 
-      <div>
-        <h1>New Release</h1>
-      </div>
+      <section id="home-center" className="px-5">
+        <section id="new-release" className=" my-5">
+          <h1 className=" text-lg font-medium py-2">New Release</h1>
+          <div>
+            <Image
+              src="/images/product/pexels-eberhardgross-2086361.jpg"
+              width={100}
+              height={100}
+            />
+          </div>
+        </section>
+
+        <section>
+          <h1 className=" text-lg font-medium py-2">Ideas For You</h1>
+          <div>
+            <Image src={pic1} alt="idea1" />
+            <Image src={pic2} alt="idea2" />
+          </div>
+        </section>
+
+        <section id="Collection-home">
+          <h1 className=" text-lg font-medium py-2 mt-4">
+            <Link href="/collection">Collection</Link>
+          </h1>
+
+          <div className="flex flex-wrap">
+            {products.slice(0, 4).map((product, index) => {
+              return (
+                <div className="w-1/2 pr-2 pb-5">
+                  <ProductCard product={product} />
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      </section>
     </main>
   );
 }
