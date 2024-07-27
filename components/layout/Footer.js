9934169f1cf64de07dd81dc7800f91a404ffac61
Atikah <atikahnazir6@gmail.com>
2024-07-27 13:10:36 +0800

    layout

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import whatsapp from "../../public/images/icon/bxl-whatsapp.svg";
+import twitter from "../../public/images/icon/bxl-twitter.svg";
+import instagram from "../../public/images/icon/bxl-instagram.svg";
+
 export default function Footer() {
   return (
-    <>
-      <div>This is a footer</div>
-    </>
+    <div className="py-5">
+      <div className="flex px-5 py-8 justify-between">
+        <h1>ART STORE</h1>
+        <div className="flex space-x-2">
+          <Image src={whatsapp} alt="whatsapp" />
+          <Image src={twitter} alt="twitter" />
+          <Image src={instagram} alt="instagram" />
+        </div>
+      </div>
+
+      <div className="px-5 text-sm ">
+        <h6>About Us</h6>
+        <h6>Terms and Conditions</h6>
+        <h6>Shipment</h6>
+        <h6>Order</h6>
+        <h6>Contact Us</h6>
+      </div>
+    </div>
   );
 }
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,18 +2,25 @@ import Image from "next/image";
 import menu from "../../public/images/icon/bx-menu.svg";
 import user from "../../public/images/icon/bx-user-circle.svg";
 import cart from "../../public/images/icon/bx-cart.svg";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <>
       <div className="flex justify-between px-10 py-6">
         <div className="flex items-center">
-          <Image src={menu} alt="menu" />
-          <div>ART STORE</div>
+          <Image src={menu} alt="menu" className="" />
+          <div className="pl-4">
+            <Link href="/">ART STORE</Link>
+          </div>
         </div>
         <div className="flex items-center">
-          <Image src={user} alt="user" />
-          <Image src={cart} alt="cart" />
+          <div className="pr-4">
+            <Image src={user} alt="user" />
+          </div>
+          <div>
+            <Image src={cart} alt="cart" />
+          </div>
         </div>
       </div>
     </>
