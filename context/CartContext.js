0fc5e5f7de26742e4acd44967e85630bb2715179
Atikah <atikahnazir6@gmail.com>
2024-07-29 17:51:11 +0800

    cart provider to add item

--- context/CartContext.js
+++ context/CartContext.js
@@ -1,5 +1,35 @@
+"use client";
 import { useState, useContext, createContext } from "react";
 
 //can export more than one function
 
-export const CartItems = createContext("test");
+export const CartItems = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [items, setItems] = useState([]); // get items from localstorage
+
+  // function to add items to cart
+  const currentItems = items || [];
+
+  const addItem = (product) => {
+    // find item in items storage
+    const itemExist = currentItems.find((item) => item.id == product.id);
+    if (itemExist) {
+      setItems(
+        items.map((item) => {
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        })
+      );
+    } else {
+      setItems([...items, { ...product, quantity: 1 }]);
+    }
+  };
+
+  return (
+    <CartItems.Provider value={{ items, addItem }}>
+      {children}
+    </CartItems.Provider>
+  );
+};
