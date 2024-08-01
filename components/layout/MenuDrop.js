18c4159ecb0678d9a3bc7e9c5f9d1fac6f5acdaa
Atikah <atikahnazir6@gmail.com>
2024-08-01 16:50:07 +0800

    link collection

--- components/layout/MenuDrop.js
+++ components/layout/MenuDrop.js
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 export default function MenuDrop({ close }) {
   const closeMenu = () => {
     close(false);
   };
   return (
     <>
-      <div className=" bg-white w-full py-5 absolute top-0 left-0 pl-5">
-        <div>Collection</div>
+      <div className="bg-white absolute space-y-3 w-full py-5 top-0 pl-5 left-0">
+        <div>
+          <Link href="/collection">Collection</Link>
+        </div>
         <div>Gallery</div>
         <div>My Orders</div>
         <div onClick={closeMenu}>Close</div>
